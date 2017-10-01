695fd0002b46376295862bc989437755631260d5
Yuriy Khokhulya <ykhokhulya@gmail.com>
2017-10-01 19:33:50 +0300

    [01-printf_to_uart] sample of UART using for printf

---
+++ 01-printf_to_uart/main.c
@@ -0,0 +1,123 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2017 Yuriy Khokhulya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ *all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+
+#include <stdlib.h>
+#include <stm32f4xx_hal.h>
+
+UART_HandleTypeDef g_uart_handle;
+
+static void initSystemClock();
+static void initUart();
+
+static void errorHandler();
+
+int main()
+{
+    HAL_Init();
+
+    initSystemClock();
+    initUart();
+
+    printf("System Clock Frequency = %lu Hz\n", SystemCoreClock);
+    printf("Device identifier = %lu\n", HAL_GetDEVID());
+
+    while (1)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        HAL_Delay(500);
+    }
+}
+
+void initSystemClock()
+{
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitTypeDef rcc_osc_init = {};
+    rcc_osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    rcc_osc_init.HSIState = RCC_HSI_ON;
+    rcc_osc_init.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    rcc_osc_init.PLL.PLLState = RCC_PLL_ON;
+    rcc_osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    rcc_osc_init.PLL.PLLM = 16;
+    rcc_osc_init.PLL.PLLN = 200;
+    rcc_osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+    rcc_osc_init.PLL.PLLQ = 7;
+    rcc_osc_init.PLL.PLLR = 2;
+
+    if (HAL_RCC_OscConfig(&rcc_osc_init) != HAL_OK)
+    {
+        errorHandler();
+    }
+
+    RCC_ClkInitTypeDef rcc_clk_init = {};
+    rcc_clk_init.ClockType =
+        (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 |
+         RCC_CLOCKTYPE_PCLK2);
+    rcc_clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    rcc_clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    rcc_clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+    rcc_clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&rcc_clk_init, FLASH_LATENCY_5) != HAL_OK)
+    {
+        errorHandler();
+    }
+}
+
+void initUart()
+{
+    g_uart_handle.Instance = USART2;
+    g_uart_handle.Init.BaudRate = 115200;
+    g_uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
+    g_uart_handle.Init.StopBits = UART_STOPBITS_1;
+    g_uart_handle.Init.Parity = UART_PARITY_NONE;
+    g_uart_handle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    g_uart_handle.Init.Mode = UART_MODE_TX_RX;
+    g_uart_handle.Init.OverSampling = UART_OVERSAMPLING_16;
+
+    if (HAL_UART_Init(&g_uart_handle) != HAL_OK)
+    {
+        errorHandler();
+    }
+}
+
+int __io_putchar(int ch)
+{
+    HAL_UART_Transmit(&g_uart_handle, (uint8_t*)&ch, 1, 0xFFFF);
+    return ch;
+}
+
+int __io_getchar()
+{
+    uint8_t ch;
+    HAL_StatusTypeDef status =
+        HAL_UART_Receive(&g_uart_handle, (uint8_t*)&ch, 1, HAL_MAX_DELAY);
+    return (status == HAL_OK) ? (int)ch : -1;
+}
+
+void errorHandler()
+{
+    while (1) __NOP();
+}
---
+++ 01-printf_to_uart/newlib.c
@@ -0,0 +1,185 @@
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <sys/unistd.h>
+
+#include <stm32f4xx.h>
+
+extern int __io_putchar(int ch) __attribute__((weak));
+extern int __io_getchar() __attribute__((weak));
+
+void _exit(int status)
+{
+    (void)status;
+
+    while (1)
+    {
+    }
+}
+
+int _close(int file)
+{
+    (void)file;
+
+    return -1;
+}
+
+int _execve(char* name, char** argv, char** env)
+{
+    (void)name;
+    (void)argv;
+    (void)env;
+
+    errno = ENOMEM;
+    return -1;
+}
+
+int _fork()
+{
+    errno = EAGAIN;
+    return -1;
+}
+
+int _fstat(int file, struct stat* st)
+{
+    if (STDOUT_FILENO == file || STDIN_FILENO == file || STDERR_FILENO == file)
+    {
+        st->st_mode = S_IFCHR;
+        return 0;
+    }
+
+    errno = EBADF;
+    return -1;
+}
+
+int _getpid()
+{
+    return 1;
+}
+
+int _isatty(int file)
+{
+    if (file == STDOUT_FILENO || file == STDIN_FILENO || file == STDERR_FILENO)
+    {
+        return 1;
+    }
+
+    errno = EBADF;
+    return -1;
+}
+
+int _kill(int pid, int sig)
+{
+    (void)pid;
+    (void)sig;
+
+    errno = EINVAL;
+    return (-1);
+}
+
+int _link(char* old, char* new)
+{
+    (void)old;
+    (void)new;
+
+    errno = EMLINK;
+    return -1;
+}
+
+int _lseek(int file, int offset, int whence)
+{
+    (void)offset;
+    (void)whence;
+
+    if (STDOUT_FILENO == file || STDERR_FILENO == file)
+    {
+        return 0;
+    }
+
+    errno = EBADF;
+    return -1;
+}
+
+caddr_t _sbrk(int incr)
+{
+    extern char _ebss;
+    static char* heap_end = &_ebss;
+
+    char* prev_heap_end = heap_end;
+    char* stack = (char*)__get_MSP();
+
+    if (heap_end + incr > stack)
+    {
+        errno = ENOMEM;
+        return (caddr_t)-1;
+    }
+
+    heap_end += incr;
+    return (caddr_t)prev_heap_end;
+}
+
+int _read(int file, char* buf, int len)
+{
+    (void)len;
+
+    if (STDIN_FILENO != file)
+    {
+        errno = EBADF;
+        return -1;
+    }
+
+    int ch = __io_getchar();
+    *buf = ch;
+    return (ch == -1) ? -1 : 1;
+}
+
+int _stat(const char* filepath, struct stat* st)
+{
+    (void)filepath;
+    (void)st;
+
+    errno = EACCES;
+    return -1;
+}
+
+clock_t _times(struct tms* buf)
+{
+    (void)buf;
+
+    errno = EACCES;
+    return -1;
+}
+
+int _unlink(char* name)
+{
+    (void)name;
+
+    errno = ENOENT;
+    return -1;
+}
+
+int _wait(int* status)
+{
+    (void)status;
+
+    errno = ECHILD;
+    return -1;
+}
+
+int _write(int file, char* buf, int len)
+{
+    int i;
+
+    if (file != STDOUT_FILENO && file != STDERR_FILENO)
+    {
+        errno = EBADF;
+        return -1;
+    }
+
+    for (i = 0; i < len; i++)
+    {
+        __io_putchar(buf[i]);
+    }
+    return len;
+}
